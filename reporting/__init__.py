479010f21bc5d5c40d11bfad953e208c57d3579d
fredkingham <fredkingham@gmail.com>
2018-09-04 11:33:30 +0100

    Adds working as a downloading state, and fixes the imports of reports

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -1,7 +1,8 @@
 """
 Package definition for the reporting OPAL plugin
 """
-from reports import Report, ReportFile, ReportOption
+from reporting.reports import Report, ReportFile, ReportOption
 
 from opal.core import celery  # NOQA
+
 __all__ = ["Report", "ReportFile", "ReportOption"]
